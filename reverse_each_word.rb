c01f0d85f3c1a39e429a992e5406da5223bd9896
S. Makai Takori <makait87@gmail.com>
2019-12-18 02:54:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
-def reverse_each_word(string)
+def reverse_each_word(words)
   new_sentence = []
  
- string.split(" ").each do |word|
+ words.split(" ").each do |word|
    new_sentence << word.reverse
   
  end
